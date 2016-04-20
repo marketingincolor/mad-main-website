6f6d87e9a20d4e9e3d24fdd570044f170e10ba04
eddt <etwilbeck@marketingincolor.com>
2016-04-20 10:54:53 -0500

    add GTM code to footer of SafetyShield theme

--- wp-content/themes/madico.com-2014/footer.php
+++ wp-content/themes/madico.com-2014/footer.php
@@ -21,7 +21,15 @@ $terms_conditions = ( $section == 'window-film' ) ? get_page_link(4683) : get_st
 	</div>
 
 	<?php wp_footer(); ?>
-
+	<!-- Google Tag Manager -->
+	<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-PXJ38D"
+	                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+			new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+			j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+			'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+		})(window,document,'script','dataLayer','GTM-PXJ38D');</script>
+	<!-- End Google Tag Manager -->
 	</body>
 
 </html>
