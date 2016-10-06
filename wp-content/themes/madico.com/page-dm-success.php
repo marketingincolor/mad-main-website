ecc8040b72b33cbf62af5a3a35c73250b25c80ec
Adam Doe <adoe@marketingincolor.com>
2016-10-06 14:54:58 -0400

    Added analytics for tag manager forms

--- wp-content/themes/madico.com/page-dm-success.php
+++ wp-content/themes/madico.com/page-dm-success.php
@@ -7,7 +7,14 @@
 ?>
 <!DOCTYPE html>
 <html lang="en">
-    <head> 
+    <head>
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-KQ2BB3');</script>
+        <!-- End Google Tag Manager --> 
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <meta charset="utf-8">
         <title>Madico Window Films - Dealers</title>
@@ -62,6 +69,10 @@
         </style>
     </head>
     <body>
+        <!-- Google Tag Manager (noscript) -->
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KQ2BB3"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->
         <div class="row">
             <div class="header small-12 medium-8 medium-offset-1 columns">
              <img src="/img/dm16-land-logo.png">
